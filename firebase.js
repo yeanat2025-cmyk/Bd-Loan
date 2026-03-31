3579d035d7800ceecfc800a6e4d6965b1af136d2
yeanat2025-cmyk <yeanat2025@gmail.com>
2026-03-31 19:30:50 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,16 @@
-// Firebase CDN
-import "https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js";
-import "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth-compat.js";
-import "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore-compat.js";
-import "https://www.gstatic.com/firebasejs/9.22.1/firebase-storage-compat.js";
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyB_qWfwC0YcWHxjs16d6LnAxzKBZg0a0P0",
   authDomain: "titi-live1.firebaseapp.com",
   databaseURL: "https://titi-live1-default-rtdb.firebaseio.com",
   projectId: "titi-live1",
-  storageBucket: "titi-live1.firebasestorage.app",
+  storageBucket: "titi-live1.appspot.com",
   messagingSenderId: "184688632840",
   appId: "1:184688632840:web:85caa04f3884ca5c609dbd",
   measurementId: "G-G8WSFMJYVL"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // Services
@@ -24,7 +19,7 @@ const db = firebase.firestore();
 const storage = firebase.storage();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
-// Export globally
+// Export globally for script.js
 window.auth = auth;
 window.db = db;
 window.storage = storage;
